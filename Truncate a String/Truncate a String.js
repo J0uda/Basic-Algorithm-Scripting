662427ff95b1b0eb80c221cfc71e0d1e030f5061
Amine Jaoued <amine.jaoued123@gmail.com>
2022-01-14 14:30:33 +0100

    update a solution

--- Truncate a String/Truncate a String.js
+++ Truncate a String/Truncate a String.js
@@ -8,3 +8,5 @@ truncateString("A-tisket a-tasket A green and yellow basket", 8)
 function truncateString(str, num) {
     return str.length > num ? str.slice(0, num) + "..." : str
 }
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
